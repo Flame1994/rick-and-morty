2a094fccb47bed421ca6318d5a0a0b015c9b31a4
Ruan Haarhoff <ruan.haarhoff@gmail.com>
2021-01-17 15:28:21 +0000

    Added api routes.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\EpisodeController;
+use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('characters', [CharacterController::class, 'all']);
+Route::get('characters/{id}', [CharacterController::class, 'show']);
+Route::get('locations', [LocationController::class, 'all']);
+Route::get('locations/{id}', [LocationController::class, 'show']);
+Route::get('episodes', [EpisodeController::class, 'all']);
+Route::get('episodes/{id}', [EpisodeController::class, 'show']);
